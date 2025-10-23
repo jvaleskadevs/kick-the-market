8a68a3d947ea4cb748007ec0a8767469af444e52
jvaleskadevs <vmjune2019@gmail.com>
2025-10-23 18:52:22 +0200

    blindFixApi,noconfig

--- api/lib/config.js
+++ api/lib/config.js
@@ -3,13 +3,27 @@ export const SITE_URL = process.env.NODE_ENV === "development" ?
   "http://localhost:5173" : 
   "https://kickthemarket.vercel.app";
 
-export const FREE_KTM_SCORE_NFT_ADDRESS = "0x88D7188ce5afDb4B6148aBCFcbdCe14911E68D91";
-export const KTM_SCORE_NFT_ADDRESS = "0x6ECFf023610e5F1e8047A5fE93c1Ab7b71E78BF8";
-export const JACKPOT_ADDRESS = "0x8fe2e15D644baF41D40ba42f858c8B3b6Cb09b80";
+
+
+// ktm v3, jackpot v2, sponsors v1 - base
+export const FREE_KTM_SCORE_NFT_ADDRESS = "0x14Dabc24Af69d38701927B68E004c71169790a5d";
+export const KTM_SCORE_NFT_ADDRESS = "0x634c4e43Bec566a7704612c8dD1F7c3Dbe28BFDC";
+export const JACKPOT_ADDRESS = "0x4DFF47404766cfDf82665A40Eb822870b9A68538";
+export const SPONSORS_ADDRESS = "0x4f17a5529B4E3e20467774708435740792E460eE";
+// ktm v3, jackpot v2, sponsors v1 - baseSepolia
+//export const FREE_KTM_SCORE_NFT_ADDRESS = "0x04E3f863123AF03A69bfAa214C911b876f417c7D";
+//export const KTM_SCORE_NFT_ADDRESS = "0x23c23ee1c65c7D95144EE36ffaFA2161698B64a4";
+//export const JACKPOT_ADDRESS = "0x5667e3FEe8E7762D9515EcD65e09bC80BF116FA0";
+//export const SPONSORS_ADDRESS = "0x1108B2E244444a23829D9F92c9219eC6980242a0";
+// ktm v2, jackpot v1, sponsors v1 - baseSepolia
+//export const FREE_KTM_SCORE_NFT_ADDRESS = "0x88D7188ce5afDb4B6148aBCFcbdCe14911E68D91";
+//export const KTM_SCORE_NFT_ADDRESS = "0x6ECFf023610e5F1e8047A5fE93c1Ab7b71E78BF8";
+//export const JACKPOT_ADDRESS = "0x8fe2e15D644baF41D40ba42f858c8B3b6Cb09b80";
+//export const SPONSORS_ADDRESS = "0x1108B2E244444a23829D9F92c9219eC6980242a0";
 //export const KTM_SCORE_NFT_ADDRESS = "0x21A169686c2bcDc19Ca38d94Fcb24153cbda5B05";
 
 
 export const LIGHTHOUSE_URL = "https://gateway.lighthouse.storage/ipfs/";
 export const ENVIO_DB_ENDPOINT = process.env.NODE_ENV === "development" ? 
   "http://localhost:8080/v1/graphql" :
-  "";
+  "https://indexer.dev.hyperindex.xyz/89e854d/v1/graphql";
